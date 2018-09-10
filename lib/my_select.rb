fd403824c57d099937faf101783b4858ba2df3b0
Asaf Davidov <adavidov@conncoll.edu>
2018-09-10 22:11:17 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,6 +3,7 @@ def my_select(collection)
   index = 0
   selected_collection = []
   while index < collection.size
-    
+    selected_collection.push(yield collection[index])
+    index += 1
   end
 end
